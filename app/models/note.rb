0307edf9bb0521f597ba513ec22fb780ea3188b4
Ajay Kumar M <ajaybunty91@gmail.com>
2024-09-07 14:31:43 +0530

    Thesec changes will create a notes model and add trix editor

---
+++ app/models/note.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: notes
+#
+#  id         :bigint           not null, primary key
+#  archived   :boolean          default(FALSE)
+#  color      :string
+#  pinned     :boolean          default(FALSE)
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_notes_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+class Note < ApplicationRecord
+  belongs_to :user
+
+  has_rich_text :content
+end
